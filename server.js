63b0e8928b7f3d42807a8a1291ae93e7c42ba3cb
Gary Chan <gary.chan@c0g1t8.com>
2021-09-29 21:15:58 +0000

    passes all tests

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res, next) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
